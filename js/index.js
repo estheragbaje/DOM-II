d52d4b246371704a73eb6d15e5e36bb2264c982e
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-09-03 16:42:10 +0100

    styled the nav using mouseleave event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // Your code goes here
+const navLinks = document.querySelectorAll(".nav-link");
+const navigation = document.querySelector(".nav");
+
+const _navLinks = Array.from(navLinks);
+
+_navLinks.forEach(navLink => {
+  navLink.addEventListener("mouseover", () => {
+    navLink.style.color = "#17A2B8";
+  });
+
+  navLink.addEventListener("mouseleave", () => {
+    navLink.style.color = "#212529";
+  });
+});
+
+// navigation.addEventListener("click", () => {
+//   navigation.style.color = "red";
+// });
+
+// //Styling for the image
+// const headerImage = document.querySelector(".img-content img");
+// headerImage.addEventListener("mouseOverListener" () => {
+//     ImageHoverTooltips.displayTooltip(this);
+// })
+
+// headerImage.addEventListener("mouseOverListener" () => {
+//     ImageHoverTooltips.hideTooltip(this);
+// })
